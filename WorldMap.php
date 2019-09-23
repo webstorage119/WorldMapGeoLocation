c805dc242ae7098c138306ec1effda7280ee2128
Leonardo Russo <info@lrusso.com>
2019-09-23 17:07:05 -0300

    Update WorldMap.php

--- WorldMap.php
+++ WorldMap.php
@@ -7,10 +7,12 @@ use GeoIp2\Database\Reader;
 // Grab the latest GeoIP2 Lite Database(s): https://dev.maxmind.com/geoip/geoip2/geolite2/
 // Grab the latest geoip2.phar: https://github.com/maxmind/GeoIP2-php/releases
 
-$reader = new Reader("WorldMap/worldmap.dat");
-$record = $reader->city("186.137.166.235");
+$myIPValue = "186.137.166.235";
 
-echo "IP: " . "186.137.166.235<br/>";
+$reader = new Reader("WorldMap/worldmap.dat");
+$record = $reader->city($myIPValue);
+
+echo "IP: " . $myIPValue . "<br/>";
 echo "Country: " . $record->country->name . "<br/>";
 echo "State: " . $record->mostSpecificSubdivision->name . "<br/>";
 echo "City: " . $record->city->name . "<br/>";
